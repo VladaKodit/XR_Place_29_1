3dd7fccb62dda64b659f007c161142e8fd3219dc
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-18 03:30:49 +0700

    refact: app component, delete my test viewport

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,43 +1,5 @@
-import { useState } from 'react';
-import { Input, CheckboxInput } from '../components/input';
-
 function App() {
-  const [phoneValue, setPhoneValue] = useState('');
-  const [tgValue, setTgValue] = useState('');
-  const [emailValue, setEmailValue] = useState('');
-  const [nameValue, setNameValue] = useState('');
-
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-      <Input
-        type="phone"
-        value={phoneValue}
-        onChange={(e) => setPhoneValue(e.target.value)}
-      />
-      <Input
-        type="tg"
-        value={tgValue}
-        onChange={(e) => setTgValue(e.target.value)}
-      />
-      <Input
-        type="email"
-        value={emailValue}
-        onChange={(e) => setEmailValue(e.target.value)}
-      />
-      <Input
-        type="name"
-        value={nameValue}
-        onChange={(e) => setNameValue(e.target.value)}
-      />
-      <CheckboxInput
-        description="Я принимаю условия"
-        textLink={{
-          url: '#',
-          text: 'Политики обработки персональных данных',
-        }}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
