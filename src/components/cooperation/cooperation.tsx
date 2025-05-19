656330e703470b6d2917d7ae5f5317fa8d52f1d3
NIKI-fed <nikifedorov@mail.ru>
2025-05-19 23:47:53 +0300

    feat(card): исправлены замечания ревьюера
    изменена логика загрузки и подключения свг картинок, стили переписаны на scss

--- src/components/cooperation/cooperation.tsx
+++ src/components/cooperation/cooperation.tsx
@@ -1,12 +1,10 @@
 import styles from './cooperation.module.scss';
 import { useState } from 'react';
 
-type TСooperationProps = {
+type TСooperationCardProps = {
   title: string;
-  icon: {
-    iconDefault: string;
-    iconHover: string;
-  };
+  iconDefault: string;
+  iconHover: string;
   alt: string;
   number: number;
   content: string;
@@ -14,11 +12,12 @@ type TСooperationProps = {
 
 export const CooperationCard = ({
   title,
-  icon,
+  iconDefault,
+  iconHover,
   alt,
   number,
   content,
-}: TСooperationProps) => {
+}: TСooperationCardProps) => {
   const [showInfo, setShowInfo] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
@@ -29,31 +28,33 @@ export const CooperationCard = ({
 
   return (
     <>
-      {!showInfo ? (
-        <div
-          className={styles.card}
-          onClick={handleClick}
-          // При наведении мыши меняется состояние isHovered для изменения картинки
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <h3 className={styles.card_title}>{title}</h3>
-          <div className={styles.card_img}>
-            <img
-              className={styles.img}
-              src={isHovered ? icon.iconHover : icon.iconDefault}
-              alt={alt}
-            />
-          </div>
-        </div>
-      ) : (
-        <div className={styles.card} onClick={handleClick}>
-          <h4 className={styles.card_number}>{number}%</h4>
-          <p className={`${styles.card_title} ${styles.card_content}`}>
-            {content}
-          </p>
-        </div>
-      )}
+      <div
+        className={styles.card}
+        onClick={handleClick}
+        // При наведении мыши меняется состояние isHovered для изменения картинки
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {!showInfo ? (
+          <>
+            <h3 className={styles.card__title}>{title}</h3>
+            <div className={styles.card__img}>
+              <img
+                className={styles['card__img-icon']}
+                src={isHovered ? iconHover : iconDefault}
+                alt={alt}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <h4 className={styles.card__number}>{number}%</h4>
+            <p className={`${styles.card__title} ${styles.card__content}`}>
+              {content}
+            </p>
+          </>
+        )}
+      </div>
     </>
   );
 };
