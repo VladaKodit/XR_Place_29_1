4f384dd84576596a825d041a2fa2b3884409dd52
NIKI-fed <nikifedorov@mail.ru>
2025-05-20 00:10:22 +0300

    feat(card): очистил app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,5 @@
-// function App() {
-//   return <div></div>;
-// }
-
-// export default App;
-
-import { CooperationCard } from '../components/cooperation/cooperation';
-import iconDefault from '../assets/images/cooperationDefault1.svg';
-import iconHover from '../assets/images/cooperationHover1.svg';
-
-console.log(iconDefault);
-
 function App() {
-  return (
-    <div>
-      <CooperationCard
-        title="повышение конверсии в сайт"
-        iconDefault={iconDefault}
-        iconHover={iconHover}
-        alt="sdf"
-        number={18}
-        content="Средняя конверсия в звонок при подключении нашего виджета на сайт"
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
