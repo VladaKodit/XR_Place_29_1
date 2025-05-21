01e84c6da80d80307d89fd64571e1be9c03e924a
Evgeniy Or <dsalion44@gmail.com>
2025-05-21 18:44:49 +0500

    feat(SectionBase): after review

--- src/components/SectionBase/SectionBase.tsx
+++ src/components/SectionBase/SectionBase.tsx
@@ -1,22 +1,17 @@
-import { type ReactNode, type ReactElement } from 'react';
+import { type ReactNode } from 'react';
 import styles from './SectionBase.module.scss';
 
 type SectionProps = {
   children: ReactNode;
-  titleComponent?: ReactElement;
   containerClassName?: string;
 };
 
 export const SectionBase = ({
   children,
-  titleComponent,
   containerClassName = '',
 }: SectionProps) => {
   return (
     <div className={`${styles.wrapper} ${containerClassName}`}>
-      {titleComponent && (
-        <div className={styles.title__wrapper}>{titleComponent}</div>
-      )}
       <div className={styles.content}>{children}</div>
     </div>
   );
