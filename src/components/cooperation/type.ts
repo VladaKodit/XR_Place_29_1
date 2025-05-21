a2361a6b1e11a97c91def9b7d3eb3e9cd44ff394
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-21 14:20:49 +0700

    refactor(Cooperation): clean code

---
+++ src/components/cooperation/type.ts
@@ -0,0 +1,8 @@
+export type TСooperationCardProps = {
+  title: string;
+  iconDefault: string;
+  iconHover: string;
+  alt: string;
+  number: number;
+  content: string;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Input';
 export * from './Navbar';
 export * from './LabeledTitle';
 export * from './Button';
+export * from './Cooperation';
