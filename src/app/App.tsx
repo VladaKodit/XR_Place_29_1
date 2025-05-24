bb3e83085fb98e215ecd163022483212cc04f246
Egorich <tv.titov2015@yandex.ru>
2025-05-24 16:53:52 +0300

    feat(moved TeamSection to sections):

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import TeamSection from '../sections/TeamSection/TeamSection';
+
 function App() {
-  return <div></div>;
+  return (
+    <I18nextProvider i18n={i18n}>
+      <div>
+        <TeamSection />
+      </div>
+    </I18nextProvider>
+  );
 }
 
 export default App;
