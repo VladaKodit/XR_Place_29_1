a5f9ee63cac10a690ff85ccd22ba2df30ea06f6c
Ekaterina Agapova <ekaterinabudnikova@yandex.ru>
2025-05-25 13:32:11 +0200

    refactor(app): delete test code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,5 @@
-import { Modal } from '../components/Modal/Modal';
-import { useModal } from '@hooks';
-
 function App() {
-  const modalHook = useModal();
-  const openModal = () =>
-    modalHook.openModal(() => {
-      // вызывается после того, как модальное окно исчезло с экрана
-      console.log('модалка исчезла с экрана');
-      // переход на главную страницу
-    });
-  return (
-    <div>
-      {!modalHook.isOpen && <button onClick={openModal}>open me</button>}
-      <Modal modalHook={modalHook}>
-        <p>спасибо за ваше доверие</p>
-      </Modal>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
