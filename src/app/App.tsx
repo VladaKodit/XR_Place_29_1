6626eca3e2af89b3e9b50a0b289cf2aa2177e505
sovetkiina <sovetkiina@gmail.com>
2025-06-03 20:16:48 +0200

    feat(TeamSection): add animation
    none
    
    none

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
 import styles from './App.module.scss';
+import TeamSection from '../sections/TeamSection/TeamSection';
 
 function App() {
-  return <div className={styles.app}></div>;
+  return (
+    <div className={styles.app}>
+      <TeamSection />
+    </div>
+  );
 }
 
 export default App;
